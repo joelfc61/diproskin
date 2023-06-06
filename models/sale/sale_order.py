8f5e08e89dd77994f03fadf23b12a8b17a15792e
david <david.flores@compromisohumano.com>
2023-06-06 15:20:51 -0600

    [ADD] _product_total

--- models/sale/sale_order.py
+++ models/sale/sale_order.py
@@ -7,6 +7,12 @@ class SaleOrder(models.Model):
     pick_id = fields.Many2one('hr.employee', tracking=True, string='Surtido', store=True)
     delivery_id = fields.Many2one('hr.employee', tracking=True, string='Entrega', store=True)
     agent_id = fields.Many2one('hr.employee', tracking=True, string='Marcado', store=True)
+    product_total = fields.Integer(string='Producto Total',compute='_product_total')
 
     def action_print_so(self):
         return self.env.ref('diproskin.action_report_sale_remision').report_action(self)
+
+    def _product_total(self):
+        for order in self:
+            for product in order.order_line:
+                order.product_total+= product.product_uom_qty
